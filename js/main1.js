36b763e76dced0155545d9ff8743c3f9b13cd346
Jeremy Poole <jeremy21@gmail.com>
2016-01-14 15:12:02 -0800

    linked projects to github

--- js/main1.js
+++ js/main1.js
@@ -4,7 +4,7 @@ $(function() {
     console.log('slider was initialized');
     $('.slick-current').prev().addClass('prev');
     $('.slick-current').next().addClass('next');
-    var array = ['<h2> Project 1 </h2>','<h2> Project 2 </h2>','<h2> Project 3 </h2>','<h2> Project 4 </h2>','<h2> Project 5 </h2>','<h2> Project 6 </h2>',];
+    var array = ['<a href="http://jeremy21212121.github.io/project1/" target="_blank"><h2> Project 1 </h2></a>','<a href="http://jeremy21212121.github.io/proj-02/" target="_blank"><h2> Project 2 </h2></a>','<a href="http://jeremy21212121.github.io/project-03/" target="_blank"><h2> Project 3 </h2></a>','<a href="https://github.com/jeremy21212121/proj-04" target="_blank"><h2> Project 4 </h2></a>','<a href="https://github.com/jeremy21212121/leredbread" target="_blank"><h2> Project 5 </h2></a>','<a href="https://github.com/jeremy21212121/project6" target="_blank"><h2> Project 6 </h2></a>'];
     $('.floater').append(array[slick.currentSlide]);
     // console.log(array[slick.currentSlide]);
 });
@@ -39,7 +39,7 @@ $(function() {
   // $('.slick-slide').removeClass('prev next');
 })
 .on('afterChange', function(event, slick, currentSlide, nextSlide){
-  var array = ['<h2> Project 1 </h2>','<h2> Project 2 </h2>','<h2> Project 3 </h2>','<h2> Project 4 </h2>','<h2> Project 5 </h2>','<h2> Project 6 </h2>',];
+var array = ['<a href="http://jeremy21212121.github.io/project1/" target="_blank"><h2> Project 1 </h2></a>','<a href="http://jeremy21212121.github.io/proj-02/" target="_blank"><h2> Project 2 </h2></a>','<a href="http://jeremy21212121.github.io/project-03/" target="_blank"><h2> Project 3 </h2></a>','<a href="https://github.com/jeremy21212121/proj-04" target="_blank"><h2> Project 4 </h2></a>','<a href="https://github.com/jeremy21212121/leredbread" target="_blank"><h2> Project 5 </h2></a>','<a href="https://github.com/jeremy21212121/project6" target="_blank"><h2> Project 6 </h2></a>'];
   // console.log(array[slick.currentSlide]);
   $('.floater').append(array[slick.currentSlide]);
   $('.floater').show('fast');
